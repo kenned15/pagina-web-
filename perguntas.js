bf1ae2d8405c7d154650fdddf2326fa51e758a64
kenned15 <162182099+kenned15@users.noreply.github.com>
2024-10-07 15:50:54 -0300

    Add files via upload

--- perguntas.js
+++ perguntas.js
@@ -1,17 +1,23 @@
 criaCartao(
-    'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
+    'xarada',
+    'O que desaparece assim que você diz o nome dele?',
+    'O silencio'
 )
 
 criaCartao(
-    'Geografia',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
+    'xarada',
+    'Eu falo, mas não tenho boca. Eu ouço, mas não tenho ouvidos. Não tenho corpo, mas vivo com o vento. Quem sou eu?',
+    'o eco'
 )
 
 criaCartao(
-    'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'xarada',
+    'Quanto mais houver de mim, menos você verá. Quem sou eu?',
+    'A escuridão'
+)
+
+criaCartao(
+    'xarada',
+    'Quem me faz não diz que faz. Quem me tem não sabe que tem. E quem sabe não me quer ter de jeito nenhum. Quem sou eu?',
+    'dinheiro falso'
 )
